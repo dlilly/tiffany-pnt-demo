db8c7082e4d8e6b84227b1d2019bb8e4ca66278c
Dave Lilly <dave.lilly@commercetools.de>
2019-11-22 10:00:14 -0600

    Add services box to product info page

--- src/components/productdetail/ProductInfo.vue
+++ src/components/productdetail/ProductInfo.vue
@@ -43,6 +43,36 @@
       </div>
       <VariantSelector :sku="sku" />
       <AddToCartForm :sku="sku"/>
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog"
+  aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <iframe :src="pntURL" width="600"
+          height="400" frameborder="0" allowtransparency="true">
+        </iframe>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
       <!-- {{> catalog/add-to-wishlist-btn}}
       {{> catalog/reserve-in-store-btn}} -->
       <div class="row">
@@ -90,6 +120,9 @@ export default {
   computed: {
     matchingVariant() {
       return this.currentProduct.variant || {};
+    },
+    pntURL() {
+      return `http://medusa.mousetrap.tech/tiffany-price-and-time-guide?sku=${this.sku}&locale=${this.$store.state.country}`;
     },
   },
 
